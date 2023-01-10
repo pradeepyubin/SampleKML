be3884c6c7d0d0868199541d12b91f998a06986c
pradeep <pradeepyubin@gmail.com>
2023-01-10 12:42:17 +0530

    Project Setup

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SimpleRequestLibrary"
+include(":shared")
