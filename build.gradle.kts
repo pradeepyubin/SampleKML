83b2752c5909cd15573862cf79884b92f991777e
pradeep <pradeepyubin@gmail.com>
2023-01-11 15:06:30 +0530

    Project Setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,32 @@ plugins {
     kotlin("multiplatform").version("1.7.10").apply(false)
 }
 
+/*afterEvaluate {
+    publishing {
+        publications {
+            release(MavenPublication) {
+                *//*from components.release
+                        groupId "com.github.pradeepyubin"
+                artifactId "simpleKml"
+                version = "1.0"*//*
+            }
+        }
+    }
+}*/
+
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            register<MavenPublication>("release") {
+
+                groupId = "com.github.pradeepyubin"
+                artifactId = "simpleKml"
+                version = "1.0"
+            }
+        }
+    }
 }
